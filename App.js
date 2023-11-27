9a8dd9cd0c0bfe61a71c78689cbf5ac1c5906163
thanle95 <lengocthan95@gmail.com>
2023-11-27 11:58:11 +0700

    nativewind

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View className="flex-1 items-center justify-center bg-teal-100">
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
